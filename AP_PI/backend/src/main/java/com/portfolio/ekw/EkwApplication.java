7592125b6b6ba285cee3a6c59dc67880e7f41cb4
Eugenia <108940859+EugeKWZ@users.noreply.github.com>
2022-12-11 13:44:00 -0300

    backend folder

---
+++ AP_PI/backend/src/main/java/com/portfolio/ekw/EkwApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.ekw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkwApplication.class, args);
+	}
+
+}
---
+++ AP_PI/backend/src/test/java/com/portfolio/ekw/EkwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.ekw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
